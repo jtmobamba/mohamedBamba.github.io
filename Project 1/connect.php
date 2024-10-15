b04eee9cd55096fc0bf64a4375fca42be3c7c9db
jtmobamba <153099251+jtmobamba@users.noreply.github.com>
2024-10-15 18:28:57 +0100

    Add files via upload

---
+++ Project 1/connect.php
@@ -0,0 +1,12 @@
+<?php
+$fullname = $_POST['fullname];
+$emailaddress = $_POST['emailaddress'];
+$password = $_POST['password'];
+$birthdate = $_POST['birthdate'];
+$gender = $_POST['gender'];
+$addressa = $_POST['addressa'];
+$addressb = $_POST['addressb'];
+$city = $_POST['city'];
+$region = $_POST['region'];
+$postcode = $_POST['postcode'];
+?>
